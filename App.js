3f4efa101f0e49ec91d7bd28bb329c4416d165c4
mukhilpranesh <125287534+mukhilpranesh@users.noreply.github.com>
2023-06-28 20:16:26 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React,{Component} from "react";
+import BottomTabNavigator from "./Components/BottomTabNavigator";
+
+export default class App extends Component {
+  render() {
+    return (
+      <View>
+        <BottomTabNavigator/>
+      </View>
+    );
+  }
+}
